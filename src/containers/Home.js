832d792a60bcc029ca840326cca9cf0527a0a645
Cassandra Liau <cass@Cassandras-MacBook-Pro.local>
2020-09-30 03:35:16 +0800

    Added basic components and css layouts

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,35 @@
 import React from "react";
 
+import Header from "../components/Header";
+
+const weatherKey = `dbcf13b0db9ead2deb7165b945bafc8b`;
+
 function Home() {
+  /* Display:
+        Weather Type (ex. Cloudy)
+        Current Temperature
+        High Temperature
+        Low Temperature
+        Cloudiness
+        Humidity
+        Wind Speed
+     */
   return (
-    <div>
-      <h1>Weather App</h1>
-    </div>
+    <>
+      <Header />
+      <main className="Home">
+        <h2>Weather in Seoul</h2>
+        <div className="Weather Info">
+          <p>Weather Type: </p>
+          <p>Current Temperature: </p>
+          <p>High Temperature: </p>
+          <p>Low Temperature: </p>
+          <p>Cloudiness: </p>
+          <p>Humidity: </p>
+          <p>Wind Speed: </p>
+        </div>
+      </main>
+    </>
   );
 }
 
